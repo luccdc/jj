cf35b869c0996f46481a18502ca53bd4fac70056
Judah Sotomayor <jisotomayor@liberty.edu>
2025-10-16 01:10:05 -0400

    refactor: Bring ssh into compliance with --harsh.

--- lib/LUCCDC/jiujitsu/Commands/SSH.pm
+++ lib/LUCCDC/jiujitsu/Commands/SSH.pm
@@ -1,14 +1,13 @@
-package LUCCDC::jiujitsu::Commands::ssh;
+package LUCCDC::jiujitsu::Commands::SSH;
+use strictures 2;
 use LUCCDC::jiujitsu::Util::Logging;
 
 use LUCCDC::jiujitsu::Util::Arguments qw(&parser :patterns);
-use LUCCDC::jiujitsu::Util::systemd   qw(&check_service);
+
+use LUCCDC::jiujitsu::Util::systemd qw(&check_service);
+
 use LUCCDC::jiujitsu::Util::Linux::PerDistro
   qw(rhel_or_debian_do rhel_or_debian_return platform);
-
-#use Linux::Systemd::Daemon;
-
-# Default method, called by the containing app when the subcommand is used.
 
 sub local_ip {
     return "127.0.0.1";
@@ -27,6 +26,12 @@ my @options = (
         flag => '--host|-h',
         val  => local_ip(),
         pat  => string_pat,
+    },
+    {
+        name => 'user',
+        flag => '--user|-u',
+        val  => "root",
+        pat  => string_pat,
     }
 );
 
@@ -43,21 +48,19 @@ my $toplevel_parser = parser( \@options, \%subcommands );
 my $subcmd_parser   = parser( \@options, \%empty );
 
 sub run {
-
-    my $cmdline = join " ", @_;
+    my ($cmdline) = @_;
 
     my %arg = $toplevel_parser->($cmdline);
 
-    print $arg{'port'};
-
-    # Do something
     exit;
 }
 
 sub check {
-    my $cmdline = join " ", @_;
+    my ($cmdline) = @_;
+
     $subcmd_parser->($cmdline);
     service_check();
+    return;
 }
 
 sub service_check {
@@ -74,7 +77,7 @@ sub service_check {
 
 sub check_bind {
     print "SSH is listening on:\n";
-    for my $line ( grep( /LISTEN.*ssh/, `ss -peanuts` ) ) {
+    for my $line ( grep { /LISTEN.*ssh/ } `ss -peanuts` ) {
         my @elems = split( /\s+/, $line );
         print $elems[4], "\n";
     }
@@ -84,33 +87,26 @@ sub check_bind {
 
 sub check_fw {
 
-    sub _check_fw_rhel {
-        print error("NOT IMPLEMENTED on RHEL\n");
-    }
+    # my $rhel = sub {
+    #     print error("NOT IMPLEMENTED on RHEL\n");
+    #   }
 
-    sub _check_fw_debian {
-        print error("NOT IMPLEMENTED on Debian\n");
+    #   my $deb = sub {
+    #     print error("NOT IMPLEMENTED on Debian\n");
 
-    }
+    #   }
 
     # Check the firewall
-    rhel_or_debian_do( \&_check_fw_rhel, \&_check_fw_debian );
+    # rhel_or_debian_do( $rhel, $deb );
+    return;
 }
 
 sub check_login {
-    my $cmdline       = join " ", @_;
-    my @check_options = (
-        {
-            name => 'user',
-            flag => '--user|-u',
-            val  => "root",
-            pat  => string_pat,
-        }
-    );
+    my ($cmdline) = @_;
 
     my %subcommands = ();
     my %arg         = parser( \@options, \%subcommands )->($cmdline);
-    print "ssh $arg{'user'}" . "@" . "$arg{'host'}";
+    `ssh $arg{'user'}" . "@" . "$arg{'host'}`;
     return;
 }
 
