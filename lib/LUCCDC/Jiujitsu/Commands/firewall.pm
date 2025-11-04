5a0dc9eed4bcb01751ecaaaa808ca246964c7812
eulalia <eulalia@j2wv7g3-vm.university.liberty.edu>
2025-11-03 20:24:28 -0500

    fix: return 1 from firewall

--- lib/LUCCDC/Jiujitsu/Commands/firewall.pm
+++ lib/LUCCDC/Jiujitsu/Commands/firewall.pm
@@ -8,6 +8,33 @@ sub run{
 	my $testvar = 'ls';
 	print $testvar, "\n";
 	my @rules = `iptables -L`;
+
+	my @IPtablesRules= ();
+	my @NFtablesRules=();
+
+	@IPtablesRules = `iptables -L`;
+	@NFtablesRules = `nft list ruleset`;
+
+	if (@IPtablesRules){
+		print "@IPtablesRules,\n";
+	}
+	else{
+		print "NFtables not in use.\n";
+	} 
+
+	if (@NFtablesRules){
+		print "@NFtablesRules,\n";
+	}
+	else{ 
+		print "NFtables not in use.\n";
+	} 
+
+
+
+
+
+
+
 	if ( grep { /22/ } @rules ) {
 		print "SSH rules!\n";
 	}
@@ -15,5 +42,4 @@ sub run{
 	exit;
 
 }
-
 1;
