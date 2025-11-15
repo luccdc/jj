0590249de719cb773600983f1e84c60be0897b72
Judah Sotomayor <jisotomayor@liberty.edu>
2025-11-15 03:00:35 -0500

    feat: Add basic build infrastructure.
    This commit is obviously very large, this is because it ties together
    both a solid build system and the means to run builds using nix.
    
    The build system is Actually Portable Perl, which is very nice.
    Nix must be taught to run APPerl, but I've got it working.

--- flake.nix
+++ flake.nix
@@ -4,59 +4,138 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    cosmo = {
+      url = "https://cosmo.zip/pub/cosmocc/cosmocc-3.3.10.zip";
+      flake = false;
+    };
+
+    perlSrc = {
+      url = "file+https://github.com/Perl/perl5/archive/refs/tags/v5.36.3.tar.gz";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ flake-parts, self, ...}:
+  outputs = inputs@{ flake-parts, self, perlSrc, cosmo, ...}:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-      ];
+      imports = [];
 
+      systems = [ "x86_64-linux" ];
 
-      # Systems supported
-      systems = [ "x86_64-linux" "aarch64-linux" ];
+      perSystem = { config, pkgs, lib, system, ... }:
+        let
+          generatedPerlPackages = import ./nix/perl.nix {inherit pkgs lib;};
 
-      perSystem = { config, pkgs, lib, system, ... }: {
-        _module.args.pkgs = import self.inputs.nixpkgs {
-          inherit system;
-          config.allowUnfreePredicate = pkg:
-            builtins.elem (lib.getName pkg) [
-              "vagrant"
-            ];
-        };
+          perlDeps = with  pkgs.perl540Packages; [
+            ModulePath
+            FileGrep
+            strictures
+          ];
 
-        devShells.default =
-          let
-            generatedPerlPackages = import ./nix/perl.nix {inherit pkgs lib;};
+          apperl-link = pkgs.writeShellScriptBin "apperl-link" ''
+rm -fr ./.apperl/nix-links && echo "Removing previous links directory..."
+mkdir -p ./.apperl/nix-links && echo "Creating links directory..."
+echo -e "Creating links to CPAN sources...\ninstall_modules entries:"
+${lib.concatMapStringsSep "\n" (p: ''ln -s ${toString p.out.src} ./.apperl/nix-links/${p.pname}-${p.version}.tar.gz
+                                     echo -e '\t".apperl/nix-links/${p.pname}-${p.version}.tar.gz"','')  perlDeps}
+'';
+
+          jiujitsu = let
+            perlUrl = "file+https://github.com/Perl/perl5/archive/refs/tags/v5.36.3.tar.gz";
+            apperl-project = lib.recursiveUpdate (lib.importJSON ./apperl-project.json) {
+              apperl_configs.jiujitsu = {
+                perl_url = perlUrl;
+              };
+            };
+
+            apperl-site.cosmocc = "${cosmo}";
           in
-            pkgs.mkShell {
+            pkgs.stdenv.mkDerivation rec {
+              pname = "jiujitsu";
+              src = ./.;
+              version = "0.1.0";
+
+              buildInputs = [
+                apperl-link
+                pkgs.perl
+                generatedPerlPackages.PerlDistAPPerl
+              ];
+              nativeBuildInputs = perlDeps;
+
+              outputs = ["out"];
+
+              HOME = "./fake-home";
+
+              dontFixup = true;
+
+              preConfigure = ''
+                mkdir -p $HOME/.config/apperl/ .apperl/o
+                cp ${(pkgs.writers.writeJSON "apperl-project.json" apperl-project)} ./apperl-project.json
+                cp ${(pkgs.writers.writeJSON "site.json" apperl-site)} $HOME/.config/apperl/site.json
+                cp ${perlSrc} .apperl/o/${baseNameOf apperl-project.apperl_configs.jiujitsu.perl_url}
+                apperl-link
+              '';
+
+              configurePhase = ''
+                runHook preConfigure
+                apperlm configure
+              '';
+
+              buildPhase = ''
+                apperlm build
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp ./${apperl-project.apperl_configs.jiujitsu.dest} $out/bin
+                cp ./${apperl-project.apperl_configs.jiujitsu.dest}.dbg $out/bin
+              '';
+            };
+
+        in
+          {
+            _module.args.pkgs = import self.inputs.nixpkgs {
+              inherit system;
+              config.allowUnfreePredicate = pkg:
+                builtins.elem (lib.getName pkg) [
+                  "vagrant"
+                ];
+            };
+
+            packages = {
+              inherit jiujitsu;
+            };
+
+            devShells.default = pkgs.mkShell {
               name = "jiujitsu";
               buildInputs = with pkgs; [
                 vagrant
+
+                # CLI Tools
                 perl
-                (pkgs.perl540.withPackages (p-pkgs: with p-pkgs; [
-                  PLS
-                  TryTiny
-                  ModulePath
-                  CPANPLUS
-                  FileGrep
+                perl540Packages.PerlCritic
+                perl540Packages.PLS
+                generatedPerlPackages.PerlDistAPPerl
+                apperl-link
+
+                # Needed for the CPAN generator script
+                (perl540.withPackages(p-pkgs: with p-pkgs; [
                   AppFatPacker
                   GetoptLongDescriptive
                   LogLog4perl
-                  perlcritic
+                  Readonly
+                  CPANPLUS
                 ]))
-              ] ++ generatedPerlPackages;
+
+              ] ++ perlDeps;
 
               # Local dev demands we extend PERL5LIB to include our local library.
-              # Will not work for flake builds.
-              # TODO Determine robust path-setting for final build.
+              # apperl-link ensures we can also run local builds.
               shellHook = ''
                 export PERL5LIB="$(pwd)/lib/:$PERL5LIB"
-              '';
+                  apperl-link
+'';
             };
-      };
+          };
     };
 }
